a33823b52a2c78a9a4db2c4495827ee085fb51d0
devilfishcn <devilfishcn@126.com>
2017-10-30 12:47:26 +0800

    输入输出捕获

--- helloworld.py
+++ helloworld.py
@@ -4,22 +4,17 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def initialize(self):
         self.write('begin init!')
-        
-    def get(self,name=''):
-        self.write("hello world!\n")
-        self.write(name)
-        
     def get_argument(self,name,age):
         self.write(name)
         self.write(name)
-        
+    def get(self,name=''):
+        self.write("hello world!\n")
+        self.write(name)
 def make_app():
     return tornado.web.Application([
         (r"/",MainHandler),
         (r"/name=(\S+)&age=",MainHandler),
         ])
-    
-    
 if __name__=="__main__":
     app=make_app()
     app.listen(8001,'127.0.0.1')
