56db4b25b8f12bcbb29c748288dab68e93eefedd
devilfishcn <devilfishcn@126.com>
2017-10-30 13:34:58 +0800

    增加静态文件

--- helloworld.py
+++ helloworld.py
@@ -3,19 +3,26 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def initialize(self):
-        self.write('begin init!')
-        
-    def get_query_argument(self,name):
+        self.write('begin init!') 
+        self.write('\t\n')
+    def get(self):
+        self.set_header('name', 'value')
+        self.write("hello world!")
+        self.write('\n')
+        name = self.get_argument('name')
         self.write(name)
-        
-#     def get(self,name=''):
-#         self.write("hello world!\n")
-#         self.write(name)
+        self.write('\n')
+        age = self.get_argument('age')
+        self.write(age)
+        self.write('\n')
+        address={'xiaoqu':'shanyucheng'}
+        self.redirect('./template.html', address)
 def make_app():
     return tornado.web.Application([
         (r"/",MainHandler),
-        (r"/name=(\S+)&age=",MainHandler),
-        ])
+        (r"/?name=(\S+)&age=",MainHandler),
+        ],
+        static_path='/root/github/tornado')
 if __name__=="__main__":
     app=make_app()
     app.listen(8001,'127.0.0.1')
