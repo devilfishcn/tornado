ea9fb582866e44053877a8be1e358e187e8ffbcd
devilfishcn <devilfishcn@126.com>
2017-12-09 11:02:07 +0800

    测试supervisor

---
+++ supervisors/tornado2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import tornado.ioloop
+import tornado.web
+import common
+from common import get_args_tuple
+class MainHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.write('begin init!') 
+        self.write('\t\n')
+    def get(self):
+        self.set_header('name', 'value')
+        self.write("hello world!")
+        self.write('\n')
+def make_app():
+    return tornado.web.Application([
+        (r"/",MainHandler),
+        ])
+
+if __name__=="__main__":
+    app=make_app()
+    app.listen(8000,'127.0.0.1')
+    tornado.ioloop.IOLoop.current().start()
